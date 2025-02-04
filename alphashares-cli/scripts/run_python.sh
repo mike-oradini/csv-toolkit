ab0b590b4c4aaf8fb12fe29615dc38bc8d806495
mike-oradini <mikeoradini@gmail.com>
2025-02-04 15:18:45 -0500

    Update run_python.sh

--- alphashares-cli/scripts/run_python.sh
+++ alphashares-cli/scripts/run_python.sh
@@ -3,9 +3,3 @@
 if [ -z "$1" ]; then
     echo "Usage: ./run_python.sh script.py"
     exit 1
-fi
-
-source /mnt/c/Users/metam/OneDrive/Desktop/AlphaShares/alpha-dev/venv/bin/activate
-python "$1"
-deactivate
-
